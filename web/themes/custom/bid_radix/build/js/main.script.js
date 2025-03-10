1acc949714e076a422bdf364e31102111dfaf00c
Chantity Buraimoh <chantityb@Mac.lan>
2025-03-09 20:03:50 -0400

    installed search api, customized navbar, configured layout builder

--- web/themes/custom/bid_radix/build/js/main.script.js
+++ web/themes/custom/bid_radix/build/js/main.script.js
@@ -14,6 +14,19 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./components/navbar/navbar.scss":
+/*!***************************************!*\
+  !*** ./components/navbar/navbar.scss ***!
+  \***************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
 /***/ "./components/page-content/page-content.scss":
 /*!***************************************************!*\
   !*** ./components/page-content/page-content.scss ***!
@@ -8470,12 +8483,13 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 		var installedChunks = {
 /******/ 			"/build/js/main.script": 0,
 /******/ 			"build/css/main.style": 0,
-/******/ 			"components/page-navigation/page-navigation": 0,
 /******/ 			"components/page-footer/page-footer": 0,
 /******/ 			"components/page-content/page-content": 0,
+/******/ 			"components/navbar/navbar": 0,
 /******/ 			"components/hero/hero": 0,
 /******/ 			"components/professional-card/professional-card": 0,
-/******/ 			"components/page/page": 0
+/******/ 			"components/page/page": 0,
+/******/ 			"components/page-navigation/page-navigation": 0
 /******/ 		};
 /******/ 		
 /******/ 		// no chunk on demand loading
@@ -8527,14 +8541,15 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["build/css/main.style","components/page-navigation/page-navigation","components/page-footer/page-footer","components/page-content/page-content","components/hero/hero","components/professional-card/professional-card","components/page/page"], function() { return __webpack_require__("./src/js/main.script.js"); })
-/******/ 	__webpack_require__.O(undefined, ["build/css/main.style","components/page-navigation/page-navigation","components/page-footer/page-footer","components/page-content/page-content","components/hero/hero","components/professional-card/professional-card","components/page/page"], function() { return __webpack_require__("./src/scss/main.style.scss"); })
-/******/ 	__webpack_require__.O(undefined, ["build/css/main.style","components/page-navigation/page-navigation","components/page-footer/page-footer","components/page-content/page-content","components/hero/hero","components/professional-card/professional-card","components/page/page"], function() { return __webpack_require__("./components/hero/hero.scss"); })
-/******/ 	__webpack_require__.O(undefined, ["build/css/main.style","components/page-navigation/page-navigation","components/page-footer/page-footer","components/page-content/page-content","components/hero/hero","components/professional-card/professional-card","components/page/page"], function() { return __webpack_require__("./components/page-content/page-content.scss"); })
-/******/ 	__webpack_require__.O(undefined, ["build/css/main.style","components/page-navigation/page-navigation","components/page-footer/page-footer","components/page-content/page-content","components/hero/hero","components/professional-card/professional-card","components/page/page"], function() { return __webpack_require__("./components/page-footer/page-footer.scss"); })
-/******/ 	__webpack_require__.O(undefined, ["build/css/main.style","components/page-navigation/page-navigation","components/page-footer/page-footer","components/page-content/page-content","components/hero/hero","components/professional-card/professional-card","components/page/page"], function() { return __webpack_require__("./components/page-navigation/page-navigation.scss"); })
-/******/ 	__webpack_require__.O(undefined, ["build/css/main.style","components/page-navigation/page-navigation","components/page-footer/page-footer","components/page-content/page-content","components/hero/hero","components/professional-card/professional-card","components/page/page"], function() { return __webpack_require__("./components/page/page.scss"); })
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["build/css/main.style","components/page-navigation/page-navigation","components/page-footer/page-footer","components/page-content/page-content","components/hero/hero","components/professional-card/professional-card","components/page/page"], function() { return __webpack_require__("./components/professional-card/professional-card.scss"); })
+/******/ 	__webpack_require__.O(undefined, ["build/css/main.style","components/page-footer/page-footer","components/page-content/page-content","components/navbar/navbar","components/hero/hero","components/professional-card/professional-card","components/page/page","components/page-navigation/page-navigation"], function() { return __webpack_require__("./src/js/main.script.js"); })
+/******/ 	__webpack_require__.O(undefined, ["build/css/main.style","components/page-footer/page-footer","components/page-content/page-content","components/navbar/navbar","components/hero/hero","components/professional-card/professional-card","components/page/page","components/page-navigation/page-navigation"], function() { return __webpack_require__("./src/scss/main.style.scss"); })
+/******/ 	__webpack_require__.O(undefined, ["build/css/main.style","components/page-footer/page-footer","components/page-content/page-content","components/navbar/navbar","components/hero/hero","components/professional-card/professional-card","components/page/page","components/page-navigation/page-navigation"], function() { return __webpack_require__("./components/hero/hero.scss"); })
+/******/ 	__webpack_require__.O(undefined, ["build/css/main.style","components/page-footer/page-footer","components/page-content/page-content","components/navbar/navbar","components/hero/hero","components/professional-card/professional-card","components/page/page","components/page-navigation/page-navigation"], function() { return __webpack_require__("./components/navbar/navbar.scss"); })
+/******/ 	__webpack_require__.O(undefined, ["build/css/main.style","components/page-footer/page-footer","components/page-content/page-content","components/navbar/navbar","components/hero/hero","components/professional-card/professional-card","components/page/page","components/page-navigation/page-navigation"], function() { return __webpack_require__("./components/page-content/page-content.scss"); })
+/******/ 	__webpack_require__.O(undefined, ["build/css/main.style","components/page-footer/page-footer","components/page-content/page-content","components/navbar/navbar","components/hero/hero","components/professional-card/professional-card","components/page/page","components/page-navigation/page-navigation"], function() { return __webpack_require__("./components/page-footer/page-footer.scss"); })
+/******/ 	__webpack_require__.O(undefined, ["build/css/main.style","components/page-footer/page-footer","components/page-content/page-content","components/navbar/navbar","components/hero/hero","components/professional-card/professional-card","components/page/page","components/page-navigation/page-navigation"], function() { return __webpack_require__("./components/page-navigation/page-navigation.scss"); })
+/******/ 	__webpack_require__.O(undefined, ["build/css/main.style","components/page-footer/page-footer","components/page-content/page-content","components/navbar/navbar","components/hero/hero","components/professional-card/professional-card","components/page/page","components/page-navigation/page-navigation"], function() { return __webpack_require__("./components/page/page.scss"); })
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["build/css/main.style","components/page-footer/page-footer","components/page-content/page-content","components/navbar/navbar","components/hero/hero","components/professional-card/professional-card","components/page/page","components/page-navigation/page-navigation"], function() { return __webpack_require__("./components/professional-card/professional-card.scss"); })
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
